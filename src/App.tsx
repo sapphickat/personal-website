54aefb498821ef9fdd7d6c3bf3cd2a44680e729f
sxpphickat <sphhhsh@gmail.com>
2025-04-27 17:03:52 -0300

    bg transition

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,15 @@ function App() {
   const [defaultColor, setDefaultColor] = useState('text-neutral-700')
 
   return (
-    <div className='h-screen w-screen flex flex-col justify-evenly items-center text-white'>
-      <Logo setDefaultColor={setDefaultColor} />
-      <SocialLinks defaultColor={defaultColor}/>
+    <div className='relative overflow-hidden h-screen w-screen'>
+      <div id='bg-gif' className="absolute inset-0 transition-opacity opacity-10 peer-hover:opacity-100 duration-300"></div>
+
+      <div className='relative z-10 h-screen w-screen flex flex-col justify-evenly items-center text-white'>
+        <div className='peer'>
+          <Logo setDefaultColor={setDefaultColor} />
+        </div>
+        <SocialLinks defaultColor={defaultColor}/>
+      </div>
     </div>
   )
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,29 +1,48 @@
+import { useState } from "react"
+
 export default function Logo({setDefaultColor}: {setDefaultColor: Function}) {
-    const handleMouseOver = () => {
-        document.body.style.backgroundImage = `
-        linear-gradient(
-            to bottom, 
-            rgba(255, 0, 0, 0.5), 
-            rgba(0, 0, 0, 0.5)
-            ), url("/xcopy.gif")`
+
+    const bg = document.getElementById('bg-gif')
+    const names = [
+        'Sxxphxx',
+    ]
+
+    const handleMouseOver = (e: any) => {
+        // document.body.style.backgroundImage = `
+        // linear-gradient(
+        //     to bottom, 
+        //     rgba(255, 0, 0, 0.5), 
+        //     rgba(0, 0, 0, 0.5)
+        //     ), url("/xcopy.gif")`
 
         setDefaultColor('text-white')
+        console.log(names.length);
+        if (bg) {
+            bg.style.opacity = "1"
+        }
+        
+        // e.target.innerHTML = names[Math.floor(Math.random() * names.length)]
+
     } 
-    const handleMouseLeave = () => {
+    const handleMouseLeave = (e: any) => {
         document.body.style.backgroundImage = ""
         setDefaultColor('text-neutral-700')
+        // e.target.innerHTML = 'Soophia'
+        if (bg) {
+            bg.style.opacity = "0"
+        }
     } 
 
 
     return (
         <>
             <h1
-                className="text-5xl"
+                className="text-5xl text-white font-mono"
                 id="logo"
                 onMouseOver={handleMouseOver}
                 onMouseLeave={handleMouseLeave}
             >
-                Sxxphxx
+                &gt;_ Sxxphxx
             </h1>
         </>
     )
