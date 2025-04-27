860b9c4fd6bc163dd8c1da9aff279d72d500aa4c
sxpphickat <sphhhsh@gmail.com>
2025-04-27 09:23:46 -0300

    refactor: remove js from hover color

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -12,13 +12,13 @@ import {
 import { ImSoundcloud2 } from "react-icons/im";
 
 const links = [
-    { href: "https://sophia-romantica.tumblr.com", icon: FaTumblr , color: "#36465D", customClass: "rainbow-icon" },
-    { href: "https://github.com/sxpphickat", icon: FaGithub , color: "#FFFFFF", customClass: "" },
-    { href: "https://www.instagram.com/sapphic.kat/", icon: FaInstagram , color: "#FF0069", customClass: "" },
-    { href: "https://open.spotify.com/user/u008sqwwc8nr9i4gvgn4snjuy", icon: FaSpotify , color: "#1ED760", customClass: "" },
-    { href: "https://soundcloud.com/sxpphickat", icon: ImSoundcloud2 , color: "#FF5500", customClass: " rounded-md overflow-hidden" },
-    { href: "https://www.youtube.com/@soophia-studies", icon: FaYoutube , color: "#FF0000", customClass: "" },
-    { href: "https://discord.com/users/318138449259528194", icon: FaDiscord , color: "#5865F2", customClass: "" },
+    { href: "https://sophia-romantica.tumblr.com", icon: FaTumblr , color: "#36465D", customClass: "rainbow-icon " },
+    { href: "https://github.com/sxpphickat", icon: FaGithub , color: "#FFFFFF", customClass: "hover:color-github" },
+    { href: "https://www.instagram.com/sapphic.kat/", icon: FaInstagram , color: "#FF0069", customClass: "hover:color-instagram" },
+    { href: "https://open.spotify.com/user/u008sqwwc8nr9i4gvgn4snjuy", icon: FaSpotify , color: "#1ED760", customClass: "hover:color-spotify" },
+    { href: "https://soundcloud.com/sxpphickat", icon: ImSoundcloud2 , color: "#FF5500", customClass: "rounded-md overflow-hidden hover:color-soundcloud" },
+    { href: "https://www.youtube.com/@soophia-studies", icon: FaYoutube , color: "#FF0000", customClass: "hover:color-youtube" },
+    { href: "https://discord.com/users/318138449259528194", icon: FaDiscord , color: "#5865F2", customClass: "hover:color-discord" },
 ]
 
 export default function SocialLinks({defaultColor}: {defaultColor: string}) {
@@ -39,17 +39,12 @@ export default function SocialLinks({defaultColor}: {defaultColor: string}) {
 
 
 function SocialLink({href, icon: Icon, color, customClass}: {href: string, icon: any, color: string, customClass: string}) {
-    const [isHovered, setIsHovered] = useState(false)
+    // const [isHovered, setIsHovered] = useState(false)
 
     return (
         <a href={href} target="_blank" rel="noopener noreferrer">
             <Icon
                 className={`${customClass} transition-colors duration-300 ease-out`}
-                style={{
-                    color: isHovered ? color : ""
-                }}
-                onMouseOver={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
             />
         </a>
     )
