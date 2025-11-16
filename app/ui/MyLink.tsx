669e32f612f0c16e36aef5ed4c69e1574a893166
sapphickat <sphhhsh@gmail.com>
2025-11-16 11:02:30 -0300

    fix: ts error

--- app/ui/MyLink.tsx
+++ app/ui/MyLink.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import { Children } from "react";
 
-export default function MyLink({href, children, className}) {
+export default function MyLink({href, children}: { href: string, children: React.ReactNode}) {
     return (
         <Link 
-            className={className + ' dark:text-white text-black dark:hover:decoration-white hover:decoration-black transition-colors underline underline-offset-4 dark:decoration-neutral-700 decoration-neutral-300'}
-            href={href}
-            
-            >
+            className={' dark:text-white text-black dark:hover:decoration-white hover:decoration-black transition-colors underline underline-offset-4 dark:decoration-neutral-700 decoration-neutral-300'}
+            href={href}>
             {children}
         </Link>
     )
