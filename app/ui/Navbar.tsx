6749cf41603ee0ffe3969c6ea3f5e4c572f7634c
sapphickat <sphhhsh@gmail.com>
2025-11-16 21:45:43 -0300

    feat: highlight current route

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,12 +1,35 @@
+'use client'
 import ThemeToggle from "./ThemeToggle";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: 'Blog',
+        href: '/blog'
+    },
+    {
+        name: 'Gallery',
+        href: '/gallery'
+    },
+    {
+        name: 'Projects',
+        href: '/projects'
+    },
+    {
+        name: 'Contact',
+        href: '/contact'
+    },
+
+]
 
 export default function NavBar() {
+    const pathname = usePathname()
 
 
     return (
-        <div className="grid sm:flex grid-cols-2 ">
+        <div className="grid sm:flex grid-cols-2">
             <div className="sm:order-1 p-4 flex  items-center mr-auto  ">
                 <Link className="" href={'/'}>
                     <Image
@@ -20,10 +43,12 @@ export default function NavBar() {
                 </Link>
             </div>
             <div className="sm:order-2 order-3 p-4 sm:p-8 sm:gap-6 gap-3 grid sm:grid-flow-col sm:justify-end items-center  ">
-                <Link className="" href={'/blog'}>Blog</Link>
-                <Link href={'/gallery'}>Gallery</Link>
-                <Link href={'/project'}>Projects</Link>
-                <Link href={'/contact'}>Contact</Link>
+            {links.map((link) => {
+                return (
+                    <Link className={pathname === link.href ? 'dark:text-white text-black underline' : ''}
+                    key={link.name} href={link.href}>{link.name}</Link>
+                )
+            })}
             </div>
             <div className="sm:order-3 order-2 pt-8 pr-8 justify-self-end">
                 <ThemeToggle />
